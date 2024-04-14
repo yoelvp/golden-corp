dae640444ededaed8a96b03467c40490a1f0221d
yoelvp <yoelvp73@gmail.com>
2024-04-14 16:54:06 -0500

    fix: actualizar estructura

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,12 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: '#021c3f',
+				secondary: '#df242b'
+			}
+		},
 	},
 	plugins: [],
 }
