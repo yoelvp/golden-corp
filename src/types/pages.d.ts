7c64318c3c05ee4b6e26417659a1f661822f8f11
yoelvp <yoelvp73@gmail.com>
2024-06-07 11:13:16 -0500

    fix: agregar subPage

--- src/types/pages.d.ts
+++ src/types/pages.d.ts
@@ -1,5 +1,12 @@
+interface SubPage {
+  name: string
+  href: string
+  isActive?: boolean
+}
+
 export interface Page {
   name: string
   href: string
   isActive?: boolean
+  subOptions?: SubPage[]
 }
