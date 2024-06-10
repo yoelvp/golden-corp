06da0ec5b1514f3d4d3abd0db590cee9f392adf5
yoelvp <yoelvp73@gmail.com>
2024-06-10 17:08:14 -0500

    fix: eliminar clases en markdown

--- astro.config.ts
+++ astro.config.ts
@@ -1,29 +1,10 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
-import addClasses from 'rehype-add-classes'
 import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://grupocorporativogolden.com',
   integrations: [tailwind(), react()],
-  markdown: {
-    rehypePlugins: [
-      [
-        addClasses,
-        {
-          h1: 'text-zinc-800 dark:text-zinc-200 text-4xl font-semibold white-to-secondary dark:white-to-primary',
-          h2: 'text-zinc-800 dark:text-zinc-200 text-2xl font-semibold white-to-secondary dark:white-to-primary',
-          h3: 'text-zinc-800 dark:text-zinc-200 text-xl font-semibold white-to-secondary dark:white-to-primary',
-          h4: 'text-zinc-800 dark:text-zinc-200 text-lg font-semibold white-to-secondary dark:white-to-primary',
-          h5: 'text-zinc-800 dark:text-zinc-200 font-semibold white-to-secondary dark:white-to-primary',
-          h6: 'text-zinc-800 dark:text-zinc-200 font-semibold white-to-secondary dark:white-to-primary',
-          img: 'border border-slate-200 dark:border-zinc-700 rounded-xl mb-6',
-          p: 'mb-6 white-to-secondary dark:white-to-primary',
-          a: 'underline underline-offset-2 hover:text-primary decoration-primary white-to-primary',
-          ul: 'list-disc list-inside'
-        }
-      ]
-    ]
-  }
+  markdown: {}
 });
